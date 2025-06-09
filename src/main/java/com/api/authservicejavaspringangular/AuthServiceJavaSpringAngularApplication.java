a07e3a9f287ada2d0b97de083e078c8bd686bf6f
Chiziane <eddybruno43@gmail.com>
2025-06-09 10:41:00 +0200

    setup: initial project configuration

---
+++ src/main/java/com/api/authservicejavaspringangular/AuthServiceJavaSpringAngularApplication.java
@@ -0,0 +1,13 @@
+package com.api.authservicejavaspringangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServiceJavaSpringAngularApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthServiceJavaSpringAngularApplication.class, args);
+    }
+
+}
